e3355ade2ca5f4a136b1b4206bc396a1226109ed
RoshanCSawant <roshancsawant@gmail.com>
2022-11-16 17:30:14 +0530

    c

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import AdminLogin from './Pages/AdminLogin/AdminLogin.jsx';
+
+
+describe ("Login", () => {
+  test ("validate function should pass on correct input", () => {
+
+      expect ('/admin/dashboard') ;
+
+  });
+  test("validate function should fail on incorrect input", () => {
+    expect ('/admin/dashbard');
+  });
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+//
+//
+ });
